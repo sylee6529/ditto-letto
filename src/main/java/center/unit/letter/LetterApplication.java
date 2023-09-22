1824a9017a1e3b0fb7354496c3deaad56702e1f8
gimhanul <gimhanuly@gmail.com>
2023-09-23 00:24:32 +0900

    initialize

---
+++ src/main/java/center/unit/letter/LetterApplication.java
@@ -0,0 +1,13 @@
+package center.unit.letter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetterApplication.class, args);
+	}
+
+}
