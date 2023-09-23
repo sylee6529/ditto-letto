e5d1e2b2a9a45319c3a0dbe81bcb0ac4b3a6b62e
gimhanul <gimhanuly@gmail.com>
2023-09-23 14:53:10 +0900

    feat: queyr letter

--- src/main/java/center/unit/letter/infrastructure/persistence/letter/LetterRepository.java
+++ src/main/java/center/unit/letter/infrastructure/persistence/letter/LetterRepository.java
@@ -1,7 +1,11 @@
 package center.unit.letter.infrastructure.persistence.letter;
 
 import center.unit.letter.domain.letter.Letter;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LetterRepository extends CrudRepository<Letter, Long> {
+
+    @Query("select count(l) from Letter l where l.from.id = :fromId and l.to.id = :toId")
+    int countByFromAndTo(Long fromId, Long toId);
 }
--- src/main/java/center/unit/letter/presentation/letter/LetterController.java
+++ src/main/java/center/unit/letter/presentation/letter/LetterController.java
@@ -1,12 +1,16 @@
 package center.unit.letter.presentation.letter;
 
+import center.unit.letter.application.letter.QueryLetterService;
 import center.unit.letter.application.letter.SendLetterService;
 import center.unit.letter.domain.user.User;
 import center.unit.letter.presentation.letter.dto.request.SendLetterRequest;
+import center.unit.letter.presentation.letter.dto.response.LetterResponse;
 import center.unit.letter.presentation.letter.dto.response.SendLetterResponse;
 import center.unit.letter.shared.auth.AuthenticationPrincipal;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LetterController {
 
     private final SendLetterService sendLetterService;
+    private final QueryLetterService queryLetterService;
 
     @PostMapping
     public SendLetterResponse sendLetter(
@@ -25,5 +30,13 @@ public class LetterController {
             @RequestBody @Valid SendLetterRequest request
     ) {
         return sendLetterService.execute(user, request);
+    }
+
+    @GetMapping("/{letter-id}")
+    public LetterResponse queryLetter(
+            @AuthenticationPrincipal User user,
+            @PathVariable(name = "letter-id") Long id
+    ) {
+        return queryLetterService.execute(user, id);
     }
 }
