fde42291dd7032ba9bd7764379e79a0deeb8cb57
buddy <jang_taehwan@naver.com>
2023-09-24 02:54:23 +0900

    refactor: medium type to randomly

--- src/main/java/center/unit/letter/application/letter/SendLetterService.java
+++ src/main/java/center/unit/letter/application/letter/SendLetterService.java
@@ -2,12 +2,14 @@ package center.unit.letter.application.letter;
 
 import center.unit.letter.domain.letter.Letter;
 import center.unit.letter.domain.letter.type.LetterType;
+import center.unit.letter.domain.letter.type.MediumType;
 import center.unit.letter.domain.user.User;
 import center.unit.letter.domain.user.service.UserFacade;
 import center.unit.letter.infrastructure.cyworld.CyworldService;
 import center.unit.letter.infrastructure.persistence.letter.LetterRepository;
 import center.unit.letter.presentation.letter.dto.request.SendLetterRequest;
 import center.unit.letter.presentation.letter.dto.response.SendLetterResponse;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,25 +22,30 @@ public class SendLetterService {
     private final LetterRepository letterRepository;
 
     public SendLetterResponse execute(
-            User from,
-            SendLetterRequest request
+        User from,
+        SendLetterRequest request
     ) {
         Letter letter = letterRepository.save(
-                new Letter(
-                        convertText(request.getText(), request.getType()),
-                        request.getMediumType(),
-                        request.getType(),
-                        userFacade.getUser(request.getTargetPhoneNumber()),
-                        from
-                )
+            new Letter(
+                convertText(request.getText(), request.getType()),
+                getRandomMediumType(),
+                request.getType(),
+                userFacade.getUser(request.getTargetPhoneNumber()),
+                from
+            )
         );
 
         return new SendLetterResponse(letter);
     }
 
+    private MediumType getRandomMediumType() {
+        MediumType[] values = MediumType.values();
+        return values[new Random().nextInt(values.length)];
+    }
+
     private String convertText(String text, LetterType type) {
         return type.equals(LetterType.CODE) ?
-                cyworldService.execute(text) :
-                text;
+            cyworldService.execute(text) :
+            text;
     }
 }
