d98785d6345cd7d377eb231934d8da45ef11ded4
buddy <jang_taehwan@naver.com>
2023-09-23 16:14:07 +0900

    refactor: User phoneNumber to unique field

--- src/main/java/center/unit/letter/domain/user/User.java
+++ src/main/java/center/unit/letter/domain/user/User.java
@@ -24,7 +24,7 @@ public class User extends BaseTimeEntity {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String phoneNumber;
 
     @Column(nullable = false)
