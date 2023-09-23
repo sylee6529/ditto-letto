ea7f56b11c41616724fafd912142cc4baa0118c3
gimhanul <gimhanuly@gmail.com>
2023-09-24 01:24:16 +0900

    feat: update user

---
+++ src/main/java/center/unit/letter/application/user/UpdateUserService.java
@@ -0,0 +1,17 @@
+package center.unit.letter.application.user;
+
+import center.unit.letter.domain.user.User;
+import center.unit.letter.presentation.user.dto.request.UpdateUserRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UpdateUserService {
+
+    @Transactional
+    public void execute(User user, UpdateUserRequest request) {
+        user.update(request.getName(), request.getPhoneNumber());
+    }
+}
---
+++ src/main/java/center/unit/letter/presentation/user/dto/request/UpdateUserRequest.java
@@ -0,0 +1,14 @@
+package center.unit.letter.presentation.user.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserRequest {
+
+    private String name;
+    private String phoneNumber;
+}
