1927016773a73dda34e1e090095e1f216911e457
gimhanul <gimhanuly@gmail.com>
2023-09-23 16:53:59 +0900

    fix: time validation

--- src/main/java/center/unit/letter/infrastructure/persistence/letter/LetterRepository.java
+++ src/main/java/center/unit/letter/infrastructure/persistence/letter/LetterRepository.java
@@ -12,6 +12,6 @@ public interface LetterRepository extends CrudRepository<Letter, Long> {
     @Query("select count(l) from Letter l where l.from.id = :fromId and l.to.id = :toId")
     int countByFromAndTo(Long fromId, Long toId);
 
-    @Query("select l from Letter  l where l.from.id = :fromId and l.to.id = :toId")
+    @Query("select l from Letter  l where l.from.id = :fromId and l.to.id = :toId and l.arriveAt <= CURRENT_TIMESTAMP")
     List<Letter> findByFromAndTo(Long fromId, Long toId);
 }
