70d007a4aee9db8d519aa5a902234f32380f6cff
buddy <jang_taehwan@naver.com>
2023-09-24 01:34:58 +0900

    feat: login by kakao access token

--- src/main/java/center/unit/letter/presentation/auth/KakaoAuthController.java
+++ src/main/java/center/unit/letter/presentation/auth/KakaoAuthController.java
@@ -1,10 +1,13 @@
 package center.unit.letter.presentation.auth;
 
 import center.unit.letter.infrastructure.kakao.KakaoAuthService;
+import center.unit.letter.presentation.auth.dto.request.KakaoAccessTokenRequest;
 import center.unit.letter.presentation.auth.dto.response.AccessTokenResponse;
 import center.unit.letter.shared.response.SingleCommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +22,10 @@ public class KakaoAuthController {
     @GetMapping("/login")
     public SingleCommonResponse<AccessTokenResponse> login(@RequestParam("code") String code) {
         return SingleCommonResponse.ok(kakaoAuthService.requestToken(code));
+    }
+
+    @PostMapping("/login/token")
+    public SingleCommonResponse<AccessTokenResponse> loginByKakaoOAuthToken(@RequestBody KakaoAccessTokenRequest request) {
+        return SingleCommonResponse.ok(kakaoAuthService.requestTokenByKakaoAccessToken(request.kakaoAccessToken()));
     }
 }
