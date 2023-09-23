59d2c8e2f5ad73867b72072629efbb178a0769c9
buddy <jang_taehwan@naver.com>
2023-09-23 20:16:23 +0900

    feat: letter arrive check scheduler

--- src/main/java/center/unit/letter/infrastructure/persistence/letter/LetterRepository.java
+++ src/main/java/center/unit/letter/infrastructure/persistence/letter/LetterRepository.java
@@ -1,11 +1,9 @@
 package center.unit.letter.infrastructure.persistence.letter;
 
 import center.unit.letter.domain.letter.Letter;
-import center.unit.letter.domain.user.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface LetterRepository extends CrudRepository<Letter, Long> {
 
@@ -14,4 +12,7 @@ public interface LetterRepository extends CrudRepository<Letter, Long> {
 
     @Query("select l from Letter  l where l.from.id = :fromId and l.to.id = :toId and l.arriveAt <= CURRENT_TIMESTAMP")
     List<Letter> findByFromAndTo(Long fromId, Long toId);
+
+    @Query("SELECT l FROM Letter l WHERE l.arrived = false")
+    List<Letter> findAllOfNotArrived();
 }
