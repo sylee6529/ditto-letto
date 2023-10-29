7f84b7c627c175e03dfc50e0dee86a23c0f7e0e3
gimhanul <gimhanuly@gmail.com>
2023-10-29 12:31:34 +0900

    feat: 귀여니체 변환 로직 추가 (#16)

--- src/main/java/center/unit/letter/presentation/gwiyeoni/GwiyeoniRepository.java
+++ src/main/java/center/unit/letter/presentation/gwiyeoni/GwiyeoniRepository.java
@@ -3,5 +3,9 @@ package center.unit.letter.presentation.gwiyeoni;
 import center.unit.letter.domain.gwiyeoni.Gwiyeoni;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface GwiyeoniRepository extends CrudRepository<Gwiyeoni, Long> {
+
+    List<Gwiyeoni> findByTextIn(List<String> textList);
 }
--- src/test/java/center/unit/letter/application/gwiyeoni/SaveAllGwiyeoniWordServiceTest.java
+++ src/test/java/center/unit/letter/application/gwiyeoni/SaveAllGwiyeoniWordServiceTest.java
@@ -1,9 +1,11 @@
 package center.unit.letter.application.gwiyeoni;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class SaveAllGwiyeoniWordServiceTest {
 
