931973500cfe779b16c1972b67619d989c9ef9cd
sylee <sylee6529@gmail.com>
2023-11-05 14:04:25 +0900

    feat: 거리/시간 계산 로직 구현

--- src/main/java/center/unit/letter/application/letter/SendLetterService.java
+++ src/main/java/center/unit/letter/application/letter/SendLetterService.java
@@ -3,13 +3,20 @@ package center.unit.letter.application.letter;
 import center.unit.letter.domain.letter.Letter;
 import center.unit.letter.domain.letter.type.LetterType;
 import center.unit.letter.domain.letter.type.MediumType;
+import center.unit.letter.domain.medium.Medium;
 import center.unit.letter.domain.user.User;
 import center.unit.letter.domain.user.service.UserFacade;
 import center.unit.letter.infrastructure.cyworld.CyworldService;
 import center.unit.letter.infrastructure.persistence.letter.LetterRepository;
+import center.unit.letter.infrastructure.persistence.medium.MediumRepository;
 import center.unit.letter.presentation.letter.dto.request.SendLetterRequest;
 import center.unit.letter.presentation.letter.dto.response.SendLetterResponse;
+
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Random;
+
+import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,32 +27,103 @@ public class SendLetterService {
     private final CyworldService cyworldService;
     private final UserFacade userFacade;
     private final LetterRepository letterRepository;
+    private final MediumRepository mediumRepository;
 
     public SendLetterResponse execute(
         User from,
         SendLetterRequest request
     ) {
+        MediumType mediumType;
+        LocalDateTime arriveAt;
+        String imgUrl;
+
+        // 일반 타입이면, 거리로 시간을 계산하여 도착시간을 정한다
+        if (request.getEventType() == null) {
+            // 경도, 위도로 거리를 계산
+            double distance = calculateDistanceInKilometer(
+                    request.getStartLatitude(),
+                    request.getStartLongitude(),
+                    request.getEndLatitude(),
+                    request.getEndLongitude()
+            );
+
+            // 거리로 타입을 랜덤으로 설정
+            Medium medium = getMediumTypeByDistance(distance);
+            mediumType = medium.getName();
+            imgUrl = medium.getImgURL();
+
+            // 선택된 탈 것을 타고 가는데 걸리는 시간을 계산(초)
+            int travelTime = calculateTravelTime(
+                    distance,
+                    medium.getKmPerHour()
+            );
+            arriveAt = LocalDateTime.now().plusSeconds(travelTime);
+        }
+
+        // 이벤트 타입인 경우, 이벤트 타입을 검색하여 도착시간과 imgURL을 가져온다
+        else {
+            Medium eventMedium = mediumRepository.findByName(request.getEventType());
+            mediumType = eventMedium.getName();
+            arriveAt = eventMedium.getArriveAt();
+            imgUrl = eventMedium.getImgURL();
+        }
+
         Letter letter = letterRepository.save(
             new Letter(
                 convertText(request.getText(), request.getType()),
-                getRandomMediumType(),
+                mediumType,
                 request.getType(),
                 userFacade.getUser(request.getTargetPhoneNumber()),
-                from
+                from,
+                    arriveAt
             )
         );
 
-        return new SendLetterResponse(letter);
-    }
-
-    private MediumType getRandomMediumType() {
-        MediumType[] values = MediumType.values();
-        return values[new Random().nextInt(values.length)];
+        return new SendLetterResponse(letter, imgUrl);
     }
 
     private String convertText(String text, LetterType type) {
         return type.equals(LetterType.CODE) ?
             cyworldService.execute(text) :
             text;
+    }
+
+    private Medium getMediumTypeByDistance(double distance) {
+        if(distance > Medium.DEFAULT_MAX_DISTANCE) {
+            distance = Medium.DEFAULT_MAX_DISTANCE;
+        }
+        List<Medium> mediumList = mediumRepository.findAllByDistance(distance);
+
+        return mediumList.get(new Random().nextInt(mediumList.size()));
+    }
+
+    private static double calculateDistanceInKilometer(double startLat, double startLon, double endLat, double endLon){
+        final int EARTH_RADIUS = 6371; // 지구의 둘레를 360도로 나눈 값
+
+        // 각도를 라디안으로 변환
+        double dLat = Math.toRadians(endLat - startLat);
+        double dLong = Math.toRadians(endLon - startLon);
+
+        startLat = Math.toRadians(startLat);
+        endLat = Math.toRadians(endLat);
+
+        // haversine 공식 적용
+        double a = Math.pow(Math.sin(dLat / 2), 2) + Math.cos(startLat) * Math.cos(endLat) * Math.pow(Math.sin(dLong / 2), 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return EARTH_RADIUS * c;
+    }
+
+    public int calculateTravelTime(double distance, double speed) {
+        if (speed == 0) {
+            throw new IllegalArgumentException("거리는 0이 될 수 없습니다.");
+        }
+
+        double timeInHours = distance / speed;
+
+        // 초단위로 변환
+        double timeInSeconds = timeInHours * 3600;
+
+        return (int) timeInSeconds;
     }
 }
