cea548327b3cd89c8995ac1a6bccd1c61b04bebd
sylee <sylee6529@gmail.com>
2023-11-26 22:57:22 +0900

    feat: 위치 관련 에러 코드 추가 [#20]

--- src/main/java/center/unit/letter/domain/letter/exception/LetterErrorCode.java
+++ src/main/java/center/unit/letter/domain/letter/exception/LetterErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum LetterErrorCode implements ErrorCode {
     FROM_LOCATION_NOT_FOUND(HttpStatus.CONFLICT, "", "출발 위치 정보를 찾을 수 없습니다."),
     TO_LOCATION_NOT_FOUND(HttpStatus.CONFLICT, "", "도착 위치 정보를 찾을 수 없습니다."),
-    INVALID_LOCATION_VALUE(HttpStatus.BAD_REQUEST, "", "유효하지 않은 경도 혹은 위도 값입니다.");
+    INVALID_LOCATION_VALUE(HttpStatus.BAD_REQUEST, "", "유효하지 않은 경도 혹은 위도 값입니다."),
+    INVALID_LOCATION(HttpStatus.BAD_REQUEST, "", "유효하지 않은 위치 정보입니다.");
 
     private final HttpStatus status;
     private final String code;
