e6f6b63ddb41fec4356ad470739507a5e77b6cf4
taehwan0 <jang_taehwan@naver.com>
2023-12-03 17:33:27 +0900

    feat: 사용자 전화번호 수정 로그 테이블 추가

--- src/main/java/center/unit/letter/application/user/UpdateUserPhoneNumberService.java
+++ src/main/java/center/unit/letter/application/user/UpdateUserPhoneNumberService.java
@@ -1,6 +1,8 @@
 package center.unit.letter.application.user;
 
+import center.unit.letter.domain.user.PhoneNumberUpdateLog;
 import center.unit.letter.domain.user.User;
+import center.unit.letter.infrastructure.persistence.user.PhoneNumberUpdateLogRepository;
 import center.unit.letter.presentation.user.dto.request.UpdateUserPhoneNumberRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,8 +12,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UpdateUserPhoneNumberService {
 
+    private final PhoneNumberUpdateLogRepository phoneNumberUpdateLogRepository;
+
     @Transactional
     public void execute(User user, UpdateUserPhoneNumberRequest request) {
+        PhoneNumberUpdateLog updateLog = new PhoneNumberUpdateLog(
+                        user.getPhoneNumber(),
+                        request.getPhoneNumber(),
+                        user
+                );
+
+        phoneNumberUpdateLogRepository.save(updateLog);
+
         user.updatePhoneNumber(request.getPhoneNumber());
     }
 }
