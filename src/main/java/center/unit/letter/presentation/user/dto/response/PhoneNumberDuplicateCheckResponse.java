c42661c251b02c3db2cb672c1e4e79cead67bdfa
taehwan0 <jang_taehwan@naver.com>
2023-12-11 23:46:49 +0900

    refactor: phone-number 중복 체크 API response 필드명 변경

--- src/main/java/center/unit/letter/presentation/user/dto/response/PhoneNumberDuplicateCheckResponse.java
+++ src/main/java/center/unit/letter/presentation/user/dto/response/PhoneNumberDuplicateCheckResponse.java
@@ -1,8 +1,11 @@
 package center.unit.letter.presentation.user.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 @Value
 public class PhoneNumberDuplicateCheckResponse {
-    boolean isDuplicated;
+    @JsonProperty("isDuplicated")
+    boolean duplicated;
 }
