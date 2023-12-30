2a37d254a0c394cf3ee33c9601db1d2de25f281b
taehwan0 <jang_taehwan@naver.com>
2023-12-30 17:49:34 +0900

    feat: kakao oauth 회원가입 api 구성

--- src/main/java/center/unit/letter/presentation/auth/KakaoAuthController.java
+++ src/main/java/center/unit/letter/presentation/auth/KakaoAuthController.java
@@ -1,11 +1,17 @@
 package center.unit.letter.presentation.auth;
 
 import center.unit.letter.application.auth.LoginService;
+import center.unit.letter.application.auth.SignUpService;
 import center.unit.letter.domain.auth.GrantType;
 import center.unit.letter.domain.user.OAuthType;
+import center.unit.letter.domain.user.User;
 import center.unit.letter.presentation.auth.dto.request.LoginByOAuthAccessTokenRequest;
+import center.unit.letter.presentation.auth.dto.request.SignUpRequest;
 import center.unit.letter.presentation.auth.dto.response.AccessTokenResponse;
+import center.unit.letter.presentation.auth.dto.response.SignUpResponse;
+import center.unit.letter.shared.response.CommonResponse;
 import center.unit.letter.shared.response.SingleCommonResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +26,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class KakaoAuthController {
 
     private final LoginService loginService;
+    private final SignUpService signUpService;
+
+    @PostMapping("/sign-up")
+    public SingleCommonResponse<SignUpResponse> signUp(@RequestBody @Valid SignUpRequest request) {
+        User user = signUpService.execute(
+                OAuthType.KAKAO,
+                request.getGrantType(),
+                request.getKey(),
+                request.getPhoneNumber()
+        );
+
+        return CommonResponse.ok(SignUpResponse.of(user));
+    }
 
     @GetMapping("/login")
     public SingleCommonResponse<AccessTokenResponse> login(@RequestParam("code") String code) {
