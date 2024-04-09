1b2dea9b4c23effad8a7febd0c401c12b2654a04
sylee <sylee6529@gmail.com>
2024-04-09 20:38:30 +0900

    feat: 편지 조회 권한 없음 추가

--- src/main/java/center/unit/letter/domain/letter/exception/base/LetterErrorCode.java
+++ src/main/java/center/unit/letter/domain/letter/exception/base/LetterErrorCode.java
@@ -11,7 +11,8 @@ public enum LetterErrorCode implements ErrorCode {
     FROM_LOCATION_NOT_FOUND(HttpStatus.NOT_FOUND, "", "출발 위치 정보를 찾을 수 없습니다."),
     TO_LOCATION_NOT_FOUND(HttpStatus.NOT_FOUND, "", "도착 위치 정보를 찾을 수 없습니다."),
     INVALID_LOCATION(HttpStatus.BAD_REQUEST, "", "유효하지 않은 위치 값입니다."),
-    LETTER_NOT_FOUND(HttpStatus.NOT_FOUND, "", "편지를 찾을 수 없습니다.");
+    LETTER_NOT_FOUND(HttpStatus.NOT_FOUND, "", "편지를 찾을 수 없습니다."),
+    LETTER_ACCESS_DENIED(HttpStatus.FORBIDDEN, "", "권한이 없습니다.");
 
     private final HttpStatus status;
     private final String code;
