17b24f7a9dda015a7a536238d81977cce975b510
taehwan0 <jang_taehwan@naver.com>
2024-04-17 22:45:02 +0900

    refactor: 로그인 인증 실패 시(비회원) 응답 코드 추가 및 로깅 추가

--- src/main/java/center/unit/letter/shared/error/exception/GlobalErrorCode.java
+++ src/main/java/center/unit/letter/shared/error/exception/GlobalErrorCode.java
@@ -11,7 +11,7 @@ public enum GlobalErrorCode implements ErrorCode {
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "", "잘못된 요청입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "", "서버에서 오류가 발생했습니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "", "권한이 없습니다."),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"", "인증 정보가 존재하지 않습니다.");
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "-101", "인증 정보가 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String code;
