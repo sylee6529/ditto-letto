97cf90fdf69b8263b921e18b551240efd4513569
taehwan0 <jang_taehwan@naver.com>
2024-04-23 12:35:58 +0900

    feat: User 정보 반환 시 OAuth Type 추가

--- src/main/java/center/unit/letter/presentation/user/dto/response/UserResponse.java
+++ src/main/java/center/unit/letter/presentation/user/dto/response/UserResponse.java
@@ -1,5 +1,6 @@
 package center.unit.letter.presentation.user.dto.response;
 
+import center.unit.letter.domain.user.OAuthType;
 import center.unit.letter.domain.user.User;
 import lombok.Value;
 
@@ -8,9 +9,11 @@ public class UserResponse {
 
     String phoneNumber;
     String name;
+    OAuthType oAuthType;
 
     public UserResponse(User user) {
         this.phoneNumber = user.getPhoneNumber();
         this.name = user.getName();
+        this.oAuthType = user.getOauthType();
     }
 }
